15d19a2e6b3dd5a3053fbe406cdafb91600c213d
rsai-praveen <mailtome2737@gmail.com>
2026-01-07 22:34:34 +0530

    ppt first copy

--- src/webdrivermethods/GetTitleMethod_2.java
+++ src/webdrivermethods/GetTitleMethod_2.java
@@ -9,7 +9,6 @@ public class GetTitleMethod_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver chrome_driver = new SafariDriver();
-		
 		chrome_driver.get("https://www.adactinhotelapp.com/");
 		String title=chrome_driver.getTitle();
 		System.out.println(title);
--- src/webdrivermethods/GetpageSourceMethod_4.java
+++ src/webdrivermethods/GetpageSourceMethod_4.java
@@ -11,7 +11,7 @@ public class GetpageSourceMethod_4 {
 		
 		safari_driver.get("https://www.adactinhotelapp.com/");
 		
-		System.out.println(safari_driver.getCurrentUrl()); // to get the current url page which has currently focused
+		System.out.println(safari_driver.getPageSource()); // to get the current url page which has currently focused
 		safari_driver.quit();
 	}
 
