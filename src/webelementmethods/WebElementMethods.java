bfea141ca55283265a6e54bdef2295626123826f
rsai-praveen <mailtome2737@gmail.com>
2026-01-22 22:40:26 +0530

    PPT update 22-1-2026

---
+++ src/webelementmethods/WebElementMethods.java
@@ -0,0 +1,23 @@
+package webelementmethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebElementMethods {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Launch Browser
+		WebDriver driver = new SafariDriver();
+		// Access Url
+		driver.get("https://demoqa.com/");
+		//wait time for 2secs
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+		Thread.sleep(3000);
+		System.out.println("complete");
+		driver.quit();
+	}
+
+}
